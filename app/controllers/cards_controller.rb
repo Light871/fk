221dd1017f16d7925dc4e39727103b5d82656406
Light871 <light872@yandex.ru>
2021-06-12 06:20:35 +0300

    Implement cards#edit, cards#update, cards#destroy

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,12 +13,32 @@ class CardsController < ApplicationController
   def create
     @card = Card.new(card_params)
 
-    if @card.valid?
-      @card.save
+    if @card.save
       redirect_to @card
     else
       render action: 'new'
     end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    redirect_to cards_path
   end
 
   private
